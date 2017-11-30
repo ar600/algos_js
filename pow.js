69ae8900059fbf91434f8c779ef6e425358d2416
alireza <alex.80.ra@gmail.com>
2017-11-30 12:19:26 -0800

    added pow.js

---
+++ pow.js
@@ -0,0 +1,19 @@
+const pow = (x, n) => {
+  //if power is zero
+    if (!n){
+        return 1;
+    }
+    //if the number is 0
+    if(!x){
+        return 0;
+    }
+//creating temp so we do not over multiply than the result expecting
+    let temp=x;
+    for(let i=0; i<Math.abs(n)-1;i++){
+        x *= temp;
+    }
+    //Terneray Opertaor for if else statements
+    return n>0? x : 1/x;
+};
+
+console.log(pow(0,0));
